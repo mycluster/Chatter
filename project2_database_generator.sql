056f201d2cd07a75f55594cbcea5f14b6422da85
Katelyn Barnes <kbarnes@hmc.edu>
2018-10-16 17:08:06 -0500

    Made database generator

---
+++ project2_database_generator.sql
@@ -0,0 +1,106 @@
+DROP TABLE class_accesses;
+DROP TABLE usr_accesses;
+DROP TABLE access_level;
+DROP TABLE class_membership;
+DROP TABLE class_roles;
+DROP TABLE classes;
+DROP TABLE notes;
+DROP TABLE note_types;
+DROP TABLE usrs;
+DROP TABLE privs;
+
+
+CREATE TABLE privs (
+    p_id NUMBER(2),
+    p_name VARCHAR2(50),
+    CONSTRAINT privs_pk PRIMARY KEY (p_id)
+);
+
+CREATE TABLE usrs (
+    u_id NUMBER(9),
+    username VARCHAR2(50),
+    f_name VARCHAR2(30),
+    l_name VARCHAR2(30),
+    priv NUMBER(2),
+    CONSTRAINT usrs_pk PRIMARY KEY (u_id),
+    CONSTRAINT usrs_fk FOREIGN KEY (priv)
+        REFERENCES privs (p_id)
+);
+
+CREATE TABLE note_types (
+    n_id NUMBER(2),
+    n_name VARCHAR2(20),
+    CONSTRAINT note_types_pk PRIMARY KEY (n_id)
+);
+
+CREATE TABLE notes (
+    n_id NUMBER(9),
+    ownr NUMBER(9),
+    ty NUMBER(2),
+    loc VARCHAR2(100),
+    CONSTRAINT notes_pk PRIMARY KEY (n_id),
+    CONSTRAINT notes_owner_fk FOREIGN KEY (ownr)
+        REFERENCES usrs (u_id),
+    CONSTRAINT notes_type_fk FOREIGN KEY (ty)
+        REFERENCES note_types (n_id)
+);
+
+CREATE TABLE classes (
+    c_id NUMBER(9),
+    c_name VARCHAR(50),
+    CONSTRAINT classes_pk PRIMARY KEY (c_id)
+);
+
+CREATE TABLE class_roles (
+    c_id NUMBER(2),
+    c_name VARCHAR2(50),
+    CONSTRAINT class_roles_pk PRIMARY KEY (c_id)
+);
+
+CREATE TABLE class_membership (
+    c_id NUMBER(10),
+    usr NUMBER(9),
+    cls NUMBER(9),
+    c_role NUMBER(2),
+    CONSTRAINT class_membership_pk PRIMARY KEY (c_id),
+    CONSTRAINT class_membership_usr FOREIGN KEY (usr)
+        REFERENCES usrs (u_id),
+    CONSTRAINT class_membership_class FOREIGN KEY (cls)
+        REFERENCES classes (c_id),
+    CONSTRAINT class_membership_role FOREIGN KEY (c_role)
+        REFERENCES class_roles (c_id)
+);
+
+CREATE TABLE access_level (
+    a_id NUMBER(1),
+    a_name VARCHAR2(50),
+    CONSTRAINT access_level_pk PRIMARY KEY (a_id)
+);
+
+CREATE TABLE usr_accesses (
+    a_id NUMBER(10),
+    usr NUMBER(9),
+    note NUMBER(9),
+    a_level NUMBER(1),
+    CONSTRAINT usr_accesses_pk PRIMARY KEY (a_id),
+    CONSTRAINT usr_accesses_usr FOREIGN KEY (usr)
+        REFERENCES usrs (u_id),
+    CONSTRAINT usr_accesses_note FOREIGN KEY (note)
+        REFERENCES notes (n_id),
+    CONSTRAINT usr_accesses_level FOREIGN KEY (a_level)
+        REFERENCES access_level (a_id)
+);
+
+CREATE TABLE class_accesses (
+    a_id NUMBER(10),
+    cls NUMBER(9),
+    note NUMBER(9),
+    a_level NUMBER(1),
+    CONSTRAINT class_accesses_pk PRIMARY KEY (a_id),
+    CONSTRAINT class_accesses_class FOREIGN KEY (cls)
+        REFERENCES classes (c_id),
+    CONSTRAINT class_accesses_note FOREIGN KEY (note)
+        REFERENCES notes(n_id),
+    CONSTRAINT class_accesses_level FOREIGN KEY (a_level)
+        REFERENCES access_level (a_id)
+);
