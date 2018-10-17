18119a16290c134757cdabb579d92199133e4aaf
Katelyn Barnes <kbarnes@hmc.edu>
2018-10-17 18:10:42 -0500

    added more method declarations to ClassAccessDao

--- chatterJava/src/main/java/com/revature/dao/ClassAccessDao.java
+++ chatterJava/src/main/java/com/revature/dao/ClassAccessDao.java
@@ -10,5 +10,7 @@ public interface ClassAccessDao {
 	public List<ClassAccess> selectAllClassAccess();
 	public ClassAccess selectClassAccessById(Integer id);
 	public Integer updateClassAccess(ClassAccess classAccess);
+	public List<ClassAccess> getClassAccessByClassId(Integer classId);
+	public List<ClassAccess> getClassAccessByNoteId(Integer noteId);
 
 }
