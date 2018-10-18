9ad6e2108b26656abb24315553170a243abaa644
Katelyn Barnes <kbarnes@hmc.edu>
2018-10-17 21:42:10 -0500

    Made util package, and implemented getAllAccessLevels(), putting a hold on coding until decision on logging is reached

--- chatterJava/src/main/java/com/revature/dao/AccessLevelDaoImpl.java
+++ chatterJava/src/main/java/com/revature/dao/AccessLevelDaoImpl.java
@@ -2,14 +2,44 @@ package com.revature.dao;
 
 import java.util.List;
 
-import com.revature.beans.AccessLevel;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 
+import com.revature.beans.AccessLevel;
+import com.revature.util.HibernateUtil;
+
+/**
+ * 
+ * @author kbarnes
+ * 
+ * Returns a list of all AccessLevels in the database
+ * if there are none the returned List Reference is
+ * null
+ * 
+ */
 public class AccessLevelDaoImpl implements AccessLevelDao {
 
 	@Override
 	public List<AccessLevel> selectAllAccessLevel() {
-		// TODO Auto-generated method stub
-		return null;
+		// create a new session
+		Session session = HibernateUtil.getSession();
+		
+		// make a null reference to a list of accessLevels
+		List<AccessLevel> accessLevels = null;
+		
+		try {
+			// attempt to get all the access levels
+			accessLevels = session.createQuery("FROM AccessLevel").list();
+		
+		} catch (HibernateException e) {
+			// if there is a hibernate exception, catch it
+			// and print the stack trace
+			e.printStackTrace();
+		} finally {
+			// clean up after ourselves
+			session.close();
+		}
+		return accessLevels;
 	}
 
 	@Override
