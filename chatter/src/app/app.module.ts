6f9b54e7d74c5cef72531fa9f8506598eef94cb0
nanorobin <4DNanosphere@gmail.com>
2018-10-20 11:05:52 -0400

    chatterBox-server.ts, package.json, system-config.ts.

--- chatter/src/app/app.module.ts
+++ chatter/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ChatterboxComponent } from './chatterbox/chatterbox.component';
+import { UserComponent } from './user/user.component';
+import { MessageComponent } from './message/message.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatterboxComponent
+    ChatterboxComponent,
+    UserComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- chatter/src/app/chatterbox/chatterbox.component.ts
+++ chatter/src/app/chatterbox/chatterbox.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-chatterbox',
   templateUrl: './chatterbox.component.html',
   styleUrls: ['./chatterbox.component.css']
 })
-export class ChatterboxComponent implements OnInit {
+export class ChatterboxComponent extends Message {
 
-  constructor() { }
+  constructor(from: UserComponent, content: string) { }
+  super(from, content);
 
   ngOnInit() {
   }
