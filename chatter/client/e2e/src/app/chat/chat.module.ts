dc40c15378a656bb41c146e0c522c31945fcaf63
nanorobin <4DNanosphere@gmail.com>
2018-10-21 02:08:47 -0400

    created chat component, deleted unused modules.

--- chatter/client/e2e/src/app/chat/chat.module.ts
+++ chatter/client/e2e/src/app/chat/chat.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatterModule } from '../chatter/chatter.module';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ChatterModule
   ],
-  declarations: []
+  declarations: [ChatComponent]
 })
 export class ChatModule { }
