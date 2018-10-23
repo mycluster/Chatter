ffecae09473546a457446b03c2125d78d9fbb5fc
Mike L <themichaelaaron@gmail.com>
2018-10-22 21:05:50 -0500

    updated noteform

--- AngularTemplates/src/app/noteForm/noteForm.component.ts
+++ AngularTemplates/src/app/noteForm/noteForm.component.ts
@@ -7,17 +7,21 @@ import { Component, OnInit } from '@angular/core';
 export class noteFormComponent implements OnInit {
     user: any;
     message: string;
+    noteName: string;
 
     constructor() { }
 
     ngOnInit() {
+
         this.user = {
-            note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+            note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            noteName: 'Latin Notes'
         };
     }
 
     onSubmit() {
-        this.message = 'You typed: ' + this.user.note;
+        this.message = 'Note Name: '+ this.user.noteName+": " + this.user.note;
+
     }
 
 }
