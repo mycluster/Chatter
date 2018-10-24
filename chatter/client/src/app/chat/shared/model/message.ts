84ecbd190bfb896d10ee01515cf459dcb6a50c93
nanorobin <4DNanosphere@gmail.com>
2018-10-24 10:14:52 -0400

    Services, install webpack, configure webpack-config.js

--- chatter/client/src/app/chat/shared/model/message.ts
+++ chatter/client/src/app/chat/shared/model/message.ts
@@ -1,13 +1,14 @@
 import { User } from './user';
 import { Action } from './action';
 import { Timestamp } from 'rxjs';
+import { Time } from '@angular/common';
 
 export class Message{
   constructor(
   public id: number,
-  public message: string,
-  public sender: string,
+  public message: any,
+  public sender: User,
   public receiver: string,
-
+  public sentAt: Time
   ){}
 }
--- chatter/client/src/app/chat/shared/services/message.service.ts
+++ chatter/client/src/app/chat/shared/services/message.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Message } from './../model/message';
 import { User } from './../model/user';
-
+import { MessageInter } from './../model/message_interface';
+import { Time } from '@angular/common';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,37 +17,53 @@ export class MessageService {
   };
 
   selectAllMessage() {
-    return this.http.get<Message>
+    return this.http.get<Message[]>(http://localhost:8085/Chatter/selectAllMessage)
   }
 
   selectMostRecentMessage() {
     return this.http.get<Message[]>(http://localhost:8085/Chatter/selectMostRecentMessage)
   }
 
-  insertMessage(message: string) {
+  insertMessage(id:string, message: string, sender:string, receiver:string, sentAt: Time) {
     let body = new HttpParams();
     let headers = new HttpHeaders().set(
       'Content-Type', 'application/x-www-form-urlencoded'
     );
-    body = body.set('username', username);
+    body = body.set('id', id);
     body = body.set('message', message);
+    body = body.set('sender', sender);
+    body =body.set('receiver', receiver);
+    body=body.set('sentAt', sentAt)
+
+    return this.http.post("http:localhost:8085/Chatter/insertMessage", 
+    body, 
+    {headers:headers});
   }
+
+
   
-  deleteMessageById(id: number) {
+  
+  deleteMessageById(id:string, message: string, sender:string, receiver:string, sentAt: Time) {
     let body = new HttpParams();
     let headers = new HttpHeaders().set(
       'Content-Type', 'application/x-www-form-urlencoded'
     );
-    body = body.set('message', null)
+    body = body.set('message', null);
+    body = body.set('sender', null);
+    body =body.set('receiver', null);
+    body=body.set('sentAt', null)
   }
 
-  updateNote(id:number){   //do we want to include messages with the User class, then we could update a note  
+  updateNote(id:string, message: string, sender:string, receiver:string, sentAt: Time){   //do we want to include messages with the User class, then we could update a note  
     let body = new HttpParams();
     let headers = new HttpHeaders().set(
       'Content-Type', 'application/x-www-form-urlencoded'
     );
+    body = body.set('id', id);
     body = body.set('message', message);
-
+    body = body.set('sender', sender);
+    body =body.set('receiver', receiver);
+    body=body.set('sentAt', sentAt)
   }
 
 }
