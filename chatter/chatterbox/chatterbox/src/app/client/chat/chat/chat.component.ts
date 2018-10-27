c595e573e3c33cd43d7a0e3d468552619222d946
nanorobin <4DNanosphere@gmail.com>
2018-10-27 01:04:04 -0400

    Popup window enabled

--- chatter/chatterbox/chatterbox/src/app/client/chat/chat/chat.component.ts
+++ chatter/chatterbox/chatterbox/src/app/client/chat/chat/chat.component.ts
@@ -19,21 +19,12 @@ const WSP = 'https://tools.ietf.org/html/rfc6455';
 })
 export class ChatComponent implements OnInit {
   action = Action;
-  username: string;
-  username2: string;
   user: User;
-  n: 30;
+  // id = this.user.id;
+  // username = this.user.username;
+  // fName = this.user.fName;
+  // lName = this.user.lName;
   messages: Message[] = [];
-  newMessage = {
-    id: "",
-    message: "",
-    sender: "",
-    receiver: "",
-    sentAt: "",
-    edited: "",
-  }
-
-  feedback: string ="";
   messageContent: string;
   ioConnection: any;
   dialogRef: MatDialogRef<DialogUserComponent> | null;
@@ -48,11 +39,118 @@ export class ChatComponent implements OnInit {
   @ViewChild(MatList, { read: ElementRef }) matList: ElementRef;
 
   @ViewChildren(MatListItem, { read: ElementRef }) matListItems: QueryList<MatListItem>;
-  
-  constructor(private socketService: SocketService,
-    public dialog: MatDialog) { }
-  ngOnInit() {
+  constructor(private socketService: SocketService, public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.initModel();
+    // Using timeout due to https://github.com/angular/angular/issues/14748
+    setTimeout(() => {
+      this.openUserPopup(this.defaultDialogUserParams);
+    }, 0);
+  }
+
+  ngAfterViewInit(): void {
+    // subscribing to any changes in the list of items / messages
+    this.matListItems.changes.subscribe(elements => {
+      this.scrollToBottom();
+    });
   }
 
 
+  private scrollToBottom(): void {
+    try {
+      this.matList.nativeElement.scrollTop = this.matList.nativeElement.scrollHeight;
+    } catch (err) {
+    }
+  }
+
+  private initModel(): void {
+    this.user = {
+      id: this.id,
+      username: this.username,
+      fName: this.fName,
+      lName: this.lName
+    }
+     
+  }
+
+  private initIoConnection(): void {
+    this.socketService.initSocket();
+
+    this.ioConnection = this.socketService.onMessage()
+      .subscribe((message: Message) => {
+        this.messages.push(message);
+      });
+
+    this.socketService.onEvent(Event.CONNECT)
+      .subscribe(() => {
+        console.log('User has been Connected');
+      });
+
+    this.socketService.onEvent(Event.DISCONNECT)
+      .subscribe(() => {
+       console.log("User has disconnected");
+      });
+  }
+
+  public onClickUserInfo() {
+    this.openUserPopup({
+      data: {
+        username: this.user.username,
+        title: 'Edit details',
+        dialogType: DialogUserType.EDIT
+      }
+    });
+  }
+
+  private openUserPopup(params): void {
+    this.dialogRef = this.dialog.open(DialogUserComponent, params);
+    this.dialogRef.afterClosed().subscribe(paramsDialog => {
+      if (!paramsDialog) {
+        return;
+      }
+    
+
+      // this.user.username = paramsDialog.username;
+      // if (paramsDialog.dialogType === DialogUserType.NEW) {
+      //   this.initIoConnection();
+      //   this.sendNotification(paramsDialog, Action.JOINED);
+      // }else if (paramsDialog.dialogType === DialogUserType.EDIT){
+      //   this.sendNotification(paramsDialog, Action.EDIT);
+      // }
+  
+
+  });
+}
+
+  public sendMessage(message: Message): void {
+    if (!message) {
+      return;
+    }
+
+    this.socketService.send({
+      id: message.id,
+      message: message.message,
+      sender: message.sender,
+      receiver: message.receiver,
+      sentAt: message.sentAt,
+      edited: message.edited 
+      
+    });
+    this.messageContent = null;
+  }
+
+
+  // public sendNotification(params: any, action: Action, receiver: User): void {
+
+  //   if (action === Action.NEWMESSAGE) {
+  //     action = {
+  //       action: action,
+  //     }
+
+  //   }
+
+  //   this.socketService.send(action);
+
+  // }
 }
